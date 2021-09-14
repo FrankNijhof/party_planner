ff3961eeb5ea9a4d43f61c7458d7441e8e88688e
Frank Nijhof <frank_nijhof_20@hotmail.com>
2021-09-14 11:30:56 +0200

    Update main.dart
    detailspage added, select animation added

--- lib/main.dart
+++ lib/main.dart
@@ -51,9 +51,8 @@ class PartyApp extends StatefulWidget {
 
 class _PartyAppState extends State<PartyApp>{
 
-  List parties = Party.createDummyList();
-
-
+  Party _selectedparty;
+  List<Party> parties = Party.createDummyList();
   void initstate() {
     super.initState();
   }
@@ -64,48 +63,95 @@ class _PartyAppState extends State<PartyApp>{
         title: "Party app",
         home: Navigator(
           pages: [
-            MaterialPage(key: ValueKey('PartyPage'),
-              child: Scaffold(),
-            )
+            MaterialPage(
+              key: ValueKey('PartyPage'),
+              child: PartyList(
+                partyList: parties,
+                onTapped: _handlePartyTapped,
+              ),
+            ),
+            if(_selectedparty != null) PartyDetailsPage(party: _selectedparty)
+
           ],
-          onPopPage: (route, result) => route.didPop(result),
+    onPopPage: (route, result) {
+    if (!route.didPop(result)) {
+    return false;
+    }
+    setState((){
+      _selectedparty = null;
+    });
+          }
+
         )
     );
+  }
+
+  void _handlePartyTapped(Party party)
+  {
+    setState((){
+      _selectedparty = party;
+    });
   }
 
 }
 
 
-class PartyList extends StatefulWidget {
-  const PartyList({Key? key}) : super(key: key);
+class PartyList extends StatelessWidget {
+  //const PartyList({Key? key}) : super(key: key);
+  final List<Party> partyList;
+  final ValueChanged<Party> onTapped;
+
+  PartyList({
+    required this.partyList,
+    required this.onTapped,
+});
 
   @override
-  _PartyListState createState() => _PartyListState();
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          for (var party in partyList)
+            ListTile(
+              title: Text(party.getName()),
+              subtitle: Text(party.getPersons().length.toString()),
+              onTap: () => onTapped(party),
+            )
+        ],
+      ),
+    );
+  }
+
+
+
+/*  @override
+  _PartyListState createState() => _PartyListState();*/
 }
 
 
-class _PartyListState extends State<PartyList> {
+/*class _PartyListState extends State<PartyList> {
 
-   final List<Party> _partyList =Party.createDummyList();
-
+  final List<Party> partyList =Party.createDummyList();
 
   Widget _buildPartyList(){
 
-  /*  List<Person> _personList = <Person>[];
+  *//*  List<Person> _personList = <Person>[];
 
       _personList.add(Person("Frank", "424684@student.saxion.nl"));
 
       _partyList.add(Party("Lan party"));
       _partyList.add(Party.withPersons("Disco party", _personList));
-      _partyList.add(Party("Marvel party"));*/
+      _partyList.add(Party("Marvel party"));*//*
 
 
     return ListView.builder(
 
-      itemCount: _partyList.length,
+      itemCount: partyList.length,
         padding: const EdgeInsets.all(16.0),
         itemBuilder:(context, i) {
-        final party = _partyList[i];
+        final party = partyList[i];
 
         return ListTile(
           title: Text(party.getName()),
@@ -119,21 +165,63 @@ class _PartyListState extends State<PartyList> {
 
     return _buildPartyList();
   }
-}
+}*/
 
-class AttendeesPage extends StatelessWidget {
-  const AttendeesPage({Key? key}) : super(key: key);
+class PartyDetailsScreen extends StatelessWidget {
+  final Party party;
+
+  PartyDetailsScreen({
+    required this.party,
+}) : super(key: ValueKey(party));
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text("Attendees"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (party != null) ...[
+              Text(party.getName(), style: Theme.of(context).textTheme.headline6),
+              Text("Number of attendees: " + party.getPersons().length.toString(), style: Theme.of(context).textTheme.subtitle1),
+            ],
+          ],
+        ),
       ),
     );
 
   }
 
+
+
+}
+
+class PartyDetailsPage extends Page{
+  final Party party;
+
+  PartyDetailsPage({
+    required this.party,
+}): super(key: ValueKey(party));
+
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+        settings: this,
+        pageBuilder: (context, animation , animation2){
+          final tween = Tween(begin: Offset (0.0 , 0.1), end: Offset.zero);
+          final curveTween = CurveTween(curve: Curves.easeInOut);
+          return SlideTransition(
+            position: animation.drive(curveTween).drive(tween),
+            child: PartyDetailsScreen(
+              party: party,
+            ),
+          );
+        }
+    );
+  }
 }
 
